f196592b0c22a58df48762ed49956d75d400ac47
heyunpeng <heyunpeng@17220103165.local>
2015-07-07 18:33:36 +0800

    修正图片路径

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,35 +1,4 @@
 
-jQuery(document).ready(function() {
-	
-    /*
-        Fullscreen background
-    */
-    $.backstretch("bk.jpg");
-    
-    /*
-        Form validation
-    */
-    $('.login-form input[type="text"], .login-form input[type="password"], .login-form textarea').on('focus', function() {
-    	$(this).removeClass('input-error');
-    });
-    
-    $('.login-form').on('submit', function(e) {
-    	
-    	$(this).find('input[type="text"], input[type="password"], textarea').each(function(){
-    		if( $(this).val() == "" ) {
-    			e.preventDefault();
-    			$(this).addClass('input-error');
-    		}
-    		else {
-    			$(this).removeClass('input-error');
-    		}
-    	});
-    	
-    });
-    
-    
-});
-
 $(function() {
     //Show alert
     $('button[data-toggle="page-alert"]').click(function(e) {
