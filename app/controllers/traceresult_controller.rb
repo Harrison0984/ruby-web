87512d7637abf4fcd5ac2c3087c234c711c3f5e5
heyunpeng <heyunpeng@17220103165.local>
2015-07-18 16:12:08 +0800

    修正后台

--- app/controllers/traceresult_controller.rb
+++ app/controllers/traceresult_controller.rb
@@ -4,7 +4,7 @@ class TraceresultController < ApplicationController
 	def index
 		curtime = Time.new
 		tasklog = Tasklog.find_by_taskdate(curtime.strftime("%Y-%m-%d"))
-		if tasklog
+		if tasklog and session[:userid] != 0
 			@seconds = tasklog.nexttime - curtime - 8 * 3600
 			@minutes = (@seconds / 60).to_i
 			@seconds = (@seconds % 60).to_i
@@ -14,7 +14,7 @@ class TraceresultController < ApplicationController
 			@issue = tsktime.strftime("%Y%m%d")+(tasklog.currentbar).to_s
 
 			@grid = Grid.find_by_gameid(@issue)
-			@gridlogs = Tracelog.where("time > ?", (curtime - 60*60).strftime("%Y-%m-%d %H:%M"))
+			@gridlogs = Tracelog.where("time > ? and userid = ?", (curtime - 60*60).strftime("%Y-%m-%d %H:%M"), session[:userid])
 		end
 
 		if session.has_key?(:userid) and session[:userid] != 0
